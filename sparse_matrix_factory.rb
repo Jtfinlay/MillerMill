cec4b8859c1a1b2eda8fc74ac48e06facfa7b9ee
Evan DeGraff <evandegraff@gmail.com>
2014-09-28 18:13:08 -0600

    Adding band matrix creation stuff

--- sparse_matrix_factory.rb
+++ sparse_matrix_factory.rb
@@ -10,6 +10,7 @@ require 'matrix'
 require './sparse_matrix'
 require './matrix_factory'
 require './tridiag_matrix_factory'
+require './band_matrix_factory'
 
 class SparseMatrixFactory < MatrixFactory
   def initialize
@@ -20,6 +21,9 @@ class SparseMatrixFactory < MatrixFactory
     if is_tridiagonal?(matrix)
       tridiag_factory = TriDiagMatrixFactory.new
       tridiag_factory.create_matrix(matrix)
+    elsif is_banded?(matrix)
+      band_factory = BandMatrixFactory.new
+      band_factory.create_matrix(matrix)
     else
       SparseMatrix.new(matrix)
     end
@@ -35,4 +39,27 @@ class SparseMatrixFactory < MatrixFactory
     end
     return true
   end
+
+  def SparseMatrixFactory.is_banded?(matrix)
+    bandwidth = SparseMatrixFactory.calculate_bandwidth(matrix).to_f
+    if matrix.row_size.to_f / bandwidth >= 2 \
+      and matrix.column_size.to_f / bandwidth >= 2
+      return true
+    else
+      return false
+    end
+  end
+
+  def SparseMatrixFactory.calculate_bandwidth(matrix)
+    bandwidth = 0
+    matrix.row_vectors().each_with_index do |row, y|
+      row.each_with_index do |v, x|
+        if v != 0 and (x-y).abs > bandwidth
+          bandwidth = (x-y).abs
+        end
+      end
+    end
+    return bandwidth
+  end
+
 end
--- test_sparse_matrix_factory.rb
+++ test_sparse_matrix_factory.rb
@@ -4,6 +4,7 @@ require 'matrix'
 require './sparse_matrix_factory'
 require './sparse_matrix'
 require './tridiag_matrix'
+require './band_factory'
 
 
 class TestSparse < Test::Unit::TestCase
@@ -13,16 +14,21 @@ class TestSparse < Test::Unit::TestCase
     # Input is a matrix
     m_sparse = Matrix[ [0, 0, 5], [1, 0, 5], [6, 0, 0] ]
     m_diag = Matrix[ [25, 93, 0], [1, 13, 5], [0, 7, 3] ]
+    m_band = Matrix[ [25, 93, 4, 0], [1, 13, 5, 1], [0, 5, 7, 3] ]
     s = SparseMatrixFactory.create_matrix(m_sparse)
     t = SparseMatrixFactory.create_matrix(m_diag)
+    b = SparseMatrixFactory.create_matrix(m_band)
 
     # Post
     # Returns sparse matrix that is equivalent to matrix
     # Returns tridiagonal matrix if input matrix is tridiagonal
+    # Returns band matrix if input matrix is banded but not tridiagonal
     assert_equal m_sparse, s.to_matrix
     assert_equal m_diag, t.to_matrix
+    assert_equal m_band, b.to_matrix
     assert_equal SparseMatrix, s.class
     assert_equal TriDiagMatrix, t.class
+    assert_equal BandMatrix, b.class
   end
 
   def test_is_tridiagonal
@@ -35,6 +41,28 @@ class TestSparse < Test::Unit::TestCase
     # Returns true if tridiagonal, false if not
     assert(SparseMatrixFactory.is_tridiagonal?(m_diag))
     assert(!SparseMatrixFactory.is_tridiagonal?(m_not_diag))
+  end
+
+  def test_is_banded
+    # Pre
+    # Input is a matrix
+    m_band = Matrix[ [25, 93, 0, 0], [0, 13, 5, 0], [0, 0, 7, 3] ]
+    m = Matrix[ [25, 93, 1, 4], [7, 13, 5, 0], [0, 4, 7, 3] ]
+
+    # Post
+    # Returns true if matrix is banded, false if not
+    assert(SparseMatrixFactory.is_banded?(m_band))
+    assert(!SparseMatrixFactory.is_banded?(m))
+  end
+
+  def test_calculate_bandwidth
+    # Pre
+    # Input is a matrix
+    m = Matrix[ [25, 93, 0, 0], [0, 13, 5, 0], [0, 0, 7, 3] ]
+
+    # Post
+    # Returns the bandwidth of the matrix
+    assert_equal 1, SparseMatrixFactory.calculate_bandwidth(m)
   end
 
 end
