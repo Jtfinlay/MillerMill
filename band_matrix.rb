1d4cb4dfd8cbbb862a3b7085e8f715d378ce35e7
James Finlay <jtfinlay@ualberta.ca>
2014-10-01 15:16:14 -0600

    Death to the TAB!!!

--- band_matrix.rb
+++ band_matrix.rb
@@ -18,43 +18,43 @@ require './delegate_matrix'
 # The +BandMatrix+ class inherits from the +SparseMatrix+ class.
 #
 class BandMatrix < DelegateMatrix
-	attr_accessor :data
-	@bandwidth
+  attr_accessor :data
+  @bandwidth
 
-	#
-	# Creates a band matrix. Supports as parameters:
-	# Matrix
-	# 2D Array
-	#
-	def initialize(*args)
-		# TODO - This is nearly the same as SparseMatrix' method. Perhaps
-		# we should refactor?
-		if args.size == 1
-			from_matrix(args[0]) if args[0].is_a? Matrix
-			from_arrays(args[0]) if args[0].is_a? Array and args[0].first
-		end
-	end
-	
-	#
-	# Returns element (+x+,+y+) of the matrix.
-	#
-	def [](x, y)
-		return 0 if !(x-y).between?(-@bandwidth,@bandwidth)
-		return @data[y][@bandwidth+x-y]
-	end
-	
-	#
-	# Sets element (+x+,+y+) of the matrix to +v+.
-	# Ignores values that are not in the band.
-	#
-	def []=(x, y, v)
-		@data[y][@bandwidth+x-y] = v if (x-y).between?(-@bandwidth,@bandwidth)
-	end
-	
-	#
-	# Converts a matrix into the compressed 2D array format.
-	#
-	def from_matrix(matrix)
+  #
+  # Creates a band matrix. Supports as parameters:
+  # Matrix
+  # 2D Array
+  #
+  def initialize(*args)
+    # TODO - This is nearly the same as SparseMatrix' method. Perhaps
+    # we should refactor?
+    if args.size == 1
+      from_matrix(args[0]) if args[0].is_a? Matrix
+      from_arrays(args[0]) if args[0].is_a? Array and args[0].first
+    end
+  end
+  
+  #
+  # Returns element (+x+,+y+) of the matrix.
+  #
+  def [](x, y)
+    return 0 if !(x-y).between?(-@bandwidth,@bandwidth)
+    return @data[y][@bandwidth+x-y]
+  end
+  
+  #
+  # Sets element (+x+,+y+) of the matrix to +v+.
+  # Ignores values that are not in the band.
+  #
+  def []=(x, y, v)
+    @data[y][@bandwidth+x-y] = v if (x-y).between?(-@bandwidth,@bandwidth)
+  end
+  
+  #
+  # Converts a matrix into the compressed 2D array format.
+  #
+  def from_matrix(matrix)
     @bandwidth = BandMatrixFactory.calculate_bandwidth(matrix)
     @row_size = matrix.row_size
     @column_size = matrix.column_size
@@ -64,32 +64,32 @@ class BandMatrix < DelegateMatrix
     DelegateMatrix.iterate_matrix(matrix, Proc.new do |x,y,v|
       self.[]=(x,y,v)
     end)
-	end
-	
-	#
-	# Converts a 2D Array into the compressed 2D array format.
-	#
-	def from_arrays(array2d)
-		@bandwidth = BandMatrixFactory.calculate_bandwidth(array2d)
-		@row_size = array2d.size
-		@column_size = array2d.first.size
-		
+  end
+  
+  #
+  # Converts a 2D Array into the compressed 2D array format.
+  #
+  def from_arrays(array2d)
+    @bandwidth = BandMatrixFactory.calculate_bandwidth(array2d)
+    @row_size = array2d.size
+    @column_size = array2d.first.size
+    
     @data = Array.new(@row_size){Array.new(@bandwidth*2+1,0)}
-		
+    
     DelegateMatrix.iterate_matrix(array2d, Proc.new do |x,y,v|
       self.[]=(x,y,v)
     end)
-	end
-	
-	#
-	# Converts compressed 2D Array into original matrix
-	#
-	def to_matrix
-		rows = Matrix.zero(@row_size, @column_size).to_a
+  end
+  
+  #
+  # Converts compressed 2D Array into original matrix
+  #
+  def to_matrix
+    rows = Matrix.zero(@row_size, @column_size).to_a
     DelegateMatrix.iterate_matrix(rows, Proc.new do |x,y,v|
       rows[y][x] = self[x,y]
     end)
-		Matrix.rows(rows)
-	end
-	
+    Matrix.rows(rows)
+  end
+  
 end
--- test_band.rb
+++ test_band.rb
@@ -13,7 +13,7 @@ class TestBand < Test::Unit::TestCase
     
   end
   
-	def test_fromMatrix
+  def test_fromMatrix
     # Pre
     # Input is a banded Matrix
     matrix = Matrix[[1,1,1,0,0,0],[0,2,2,2,0,0],[0,0,3,3,3,0],[0,0,0,4,4,4],[0,0,0,5,5,5]]
@@ -23,43 +23,43 @@ class TestBand < Test::Unit::TestCase
     # Returns band matrix equivalent to input matrix
     expected = [[0,0,1,1,1],[0,0,2,2,2],[0,0,3,3,3],[0,0,4,4,4],[0,5,5,5,0]]
     assert_equal expected, b.data
-	end
-	
-	def test_from_arrays
+  end
+  
+  def test_from_arrays
     # Pre
     # Input is a 2d array
-		array = [[1,1,1,0,0,0],[0,2,2,2,0,0],[0,0,3,3,3,0],[0,0,0,4,4,4],[0,0,0,5,5,5]]
-		b = BandMatrixFactory.create_matrix(array)
+    array = [[1,1,1,0,0,0],[0,2,2,2,0,0],[0,0,3,3,3,0],[0,0,0,4,4,4],[0,0,0,5,5,5]]
+    b = BandMatrixFactory.create_matrix(array)
     
     # Post
     # Returns band matrix equivalent to input 2d array
     expected = [[0,0,1,1,1],[0,0,2,2,2],[0,0,3,3,3],[0,0,4,4,4],[0,5,5,5,0]]
     assert_equal expected, b.data
-	end
-	
-	def test_to_matrix
+  end
+  
+  def test_to_matrix
     # Pre
     # Input is a banded matrix
     matrix = Matrix[[1,1,1,0,0,0],[0,2,2,2,0,0],[0,0,3,3,3,0],[0,0,0,4,4,4],[0,0,0,5,5,5]]
-		b = BandMatrixFactory.create_matrix(matrix)
-		
+    b = BandMatrixFactory.create_matrix(matrix)
+    
     # Post
     # Returns Matrix object equivalent to input banded matrix
-		assert_equal matrix, b.to_matrix
-	end
-	
-	def test_Addition
+    assert_equal matrix, b.to_matrix
+  end
+  
+  def test_Addition
     # Pre
     # Input is two banded matrices
-		array = BandMatrixFactory.create_matrix([[1,1,1,0,0,0],[0,2,2,2,0,0],[0,0,3,3,3,0],[0,0,0,4,4,4],[0,0,0,5,5,5]])
-		
+    array = BandMatrixFactory.create_matrix([[1,1,1,0,0,0],[0,2,2,2,0,0],[0,0,3,3,3,0],[0,0,0,4,4,4],[0,0,0,5,5,5]])
+    
     # Post
     # Returns sum of both matrices in banded matrix format
     expected = BandMatrixFactory.create_matrix([[2,2,2,0,0,0],[0,4,4,4,0,0],[0,0,6,6,6,0],[0,0,0,8,8,8],[0,0,0,10,10,10]])
     assert_equal expected.data, (array+array).data
-	end
-	
-	def test_Subtraction
+  end
+  
+  def test_Subtraction
     # Pre
     # Input is two banded matrices
     array1 = BandMatrixFactory.create_matrix([[1,1,1,0,0,0],[0,2,2,2,0,0],[0,0,3,3,3,0],[0,0,0,4,4,4],[0,0,0,5,5,5]])
@@ -69,6 +69,6 @@ class TestBand < Test::Unit::TestCase
     # Returns difference in banded matrix format
     expected = BandMatrixFactory.create_matrix([[0,0,0,0,0,0],[0,1,1,1,0,0],[0,0,2,2,2,0],[0,0,0,3,3,3],[0,0,0,4,4,4]])
     assert_equal expected.data, (array1-array2).data
-	end
-	
+  end
+  
 end
