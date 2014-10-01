c644dd799c0c723b36601306baecb3e4ff143278
James Finlay <jtfinlay@ualberta.ca>
2014-10-01 17:19:37 -0600

    Factory & Band contracts!

--- contract_band.rb
+++ contract_band.rb
@@ -8,12 +8,20 @@ class ContractBand < Test::Unit::TestCase
     assert(b.row_size >= 0)
     assert(b.column_size >= 0)
     # All elements stored are within bandwidth
-    assert(s.data.size == s.bandwidth)
+    assert(b.data.size == row_size)
+    assert(b.data.first.size <= b.column_size) if not b.data.first.nil?
+    assert(b.bandwidth*2+1 <= b.column_size)
+    # All Numeric values
+    assert(b.data.index{
+      |r| (not r.is_a? Array) or (r.index{
+        |v| not v.is_a? Numeric
+      }
+    ).nil?}
   end
 
   def pre_initialize(*args)
     assert_equal args.size 1
-    assert(args[0]is_a? Matrix || (args[0]is_a? Array and args[0].first))
+    assert(args[0].is_a? Matrix || (args[0].is_a? Array and args[0].first.is_a? Array))
   end
 
   def post_initialize(self, *args)
@@ -22,10 +30,10 @@ class ContractBand < Test::Unit::TestCase
 
   def pre_square_brackets(self, x, y)
     assert(0 <= x <= self.row_size)
-    assert(0 <= x <= self.column_size)
+    assert(0 <= y <= self.column_size)
   end
 
-  def pre_square_brackets(self, x, y, result)
+  def post_square_brackets(self, x, y, result)
     if (x-y).between?(-self.bandwidth,self.bandwidth)
       assert_equal self.data[y][self.bandwidth+x-y], result
     else
@@ -46,6 +54,11 @@ class ContractBand < Test::Unit::TestCase
   def pre_fromMatrix(matrix)
     assert matrix.is_a? Matrix
     assert BandMatrixFactory.is_valid?(matrix)
+    assert matrix.row_vectors.index{
+      |r| (not r.is_a? Array) or (r.index{
+        |v| not.is_a? Numeric
+      }
+    }.nil?
   end
 
   def post_fromMatrix(matrix, result)
@@ -55,9 +68,15 @@ class ContractBand < Test::Unit::TestCase
 
   def pre_from_arrays(array)
     assert array.is_a? Array
+    assert assert BandMatrixFactory.is_valid?(array)
+    assert array.index{
+      |r| (not r.is_a? Array) or (r.index{
+        |v| not.is_a? Numeric
+      }
+    }.nil?
   end
 
-  def post_from_array(array, result)
+  def post_from_arrays(array, result)
     assert result.is_a? BandMatrix
     assert_equal array, result.data
   end
