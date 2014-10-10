32565a08e557101ba832c87aaff42e9a150c8a9e
James Finlay <jtfinlay@ualberta.ca>
2014-10-10 12:55:39 -0600

    Shell push

---
+++ Assignment2/p1/main.rb
@@ -0,0 +1,16 @@
+# encoding: utf-8
+#
+# = main.rb
+#
+# Driver that takes inputs and passes it through to the shell.
+#
+# Authors: Evan Degraff, James Finlay
+##
+
+# loop
+  # get_command
+  # create child/worker process
+  # Parent: Wait for worker (child) to finish
+  # Child: change job
+  # Parent: report results
+# end_loop
---
+++ Assignment2/p1/shell.rb
@@ -0,0 +1,38 @@
+# encoding: utf-8
+#
+# = shell.rb
+#
+# Shell class, which tries to run given commands.
+#
+# Authors: Evan Degraff, James Finlay
+##
+
+class Shell
+
+  # TODO Create list of valid commands
+
+  #
+  # Creates shell object.
+  #
+  def initialize()
+    # TODO
+  end
+
+  #
+  # Manage input and execute given commands
+  #
+  def execute(cmd)
+    # TODO
+    # TODO Input could be invalid
+    # TODO Input could be multi-line (separated by '\')
+    # TODO Input could be multi-command (separated by '|')
+  end
+
+  #
+  # Execute single command
+  #
+  def execute_single_command(cmd)
+    # TODO
+  end
+
+end
