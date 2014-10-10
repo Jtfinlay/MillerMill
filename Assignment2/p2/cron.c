9025bd166d3e28f6a743e00cf0cf9c79dcc432df
Evan DeGraff <evandegraff@gmail.com>
2014-10-10 16:54:16 -0600

    akhjfdksdfkhfdskghkdsfgsdf Please enter the commit message for your changes. Lines starting

--- Assignment2/p2/cron.c
+++ Assignment2/p2/cron.c
@@ -1,10 +1,14 @@
 // C code that takes params from Ruby, then manages threads and timers
 // and shit.
 #include <stdio.h>
-#include "ruby.h"
 
 int main(int argc, char * argv[])
 {
   printf("test");
   return 0;
+}
+
+void print(char * string)
+{
+  printf(string);
 }
