667965baf6c424638be8c111bfb09d89a8e9a1b5
James Finlay <jtfinlay@ualberta.ca>
2014-10-27 09:41:38 -0600

    Assignment 3 initial push

---
+++ Assignment3/driver.rb
@@ -0,0 +1,14 @@
+# encoding: utf-8
+#
+# = driver.rb
+#
+# Driver that takes inputs for multi-threaded sort
+#
+# Authors: Evan Degraff, James Finlay
+##
+
+if ARGV.length < 2
+  puts "Two arguments required: time and file name."
+else
+  # Do shit.
+end
