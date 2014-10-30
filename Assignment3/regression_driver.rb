cba2cfbe9b2491377558c77b11f0192242b8b016
jtfinlay <jtfinlay@ualberta.ca>
2014-10-30 14:44:22 -0600

    Regression results

--- Assignment3/regression_driver.rb
+++ Assignment3/regression_driver.rb
@@ -11,8 +11,14 @@ require './regression.rb'
 
 include Regression
 
-if ARGV.length < 2
-  puts "Two arguments required: output file and series of threads"
+if ARGV.length < 1
+  puts "Argument required: output file"
+elsif ARGV.length == 1
+  File.open(ARGV[0], 'w'){|file|
+    (1..200).each{|thread_count|
+       file.puts("#{thread_count},#{thread_task(thread_count)}")
+    }
+  }
 else
   File.open(ARGV[0], 'w'){ |file|
     ARGV[1..-1].each{|thread_count|
