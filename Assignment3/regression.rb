524cedfe4a84848f40ac3bb54f83440add17ae63
Evan DeGraff <evandegraff@gmail.com>
2014-10-30 14:36:11 -0600

    Fixing

--- Assignment3/regression.rb
+++ Assignment3/regression.rb
@@ -3,13 +3,13 @@ module Regression
   def thread_task(num_threads)
     work_per_thread = 100000 / num_threads
     threads = []
-    t = Time.now
+    t1 = Time.now
     (0..num_threads).each{
       |x| threads << Thread.new{(0..work_per_thread).each{Math.sqrt(100000) }}
     }
     threads.each{|t| t.join}
     t2 = Time.now
-    return t2 - t
+    return t2 - t1
   end
 
 end
