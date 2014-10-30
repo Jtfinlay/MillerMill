a1d26d2010c82c06ec795db0508298613f2fbc78
Evan DeGraff <evandegraff@gmail.com>
2014-10-30 14:32:26 -0600

    THread program regression thing

---
+++ Assignment3/regression.rb
@@ -0,0 +1,15 @@
+module Regression
+
+  def thread_task(num_threads)
+    work_per_thread = 100000 / num_threads
+    threads = []
+    t = Time.now
+    (0..num_threads).each{
+      |x| threads << Thread.new{(0..work_per_thread).each{Math.sqrt(100000) }}
+    }
+    threads.each{|t| t.join}
+    t2 = Time.now
+    return t2 - t
+  end
+
+end
