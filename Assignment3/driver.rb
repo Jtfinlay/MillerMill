e508f86e261fdc295273b1a341abe51cffd3af00
Evan DeGraff <evandegraff@gmail.com>
2014-11-06 15:08:27 -0700

    Driver stuff

--- Assignment3/driver.rb
+++ Assignment3/driver.rb
@@ -13,9 +13,5 @@ if ARGV.length < 2
   puts "Two arguments required: time and objects."
 else
   sorter = MergeSort.new
-<<<<<<< HEAD
-  sorter.start(ARGV[0].to_f, ARGV[1..-1])
-=======
-  puts sorter.sort(ARGV[0].to_i, ARGV[1..-1])
->>>>>>> b376c16de701f9feeb015a5da3b6b4ff0d975f23
+  puts sorter.start(ARGV[0].to_i, ARGV[1..-1])
 end
