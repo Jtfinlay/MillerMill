aac8158eb17820515f313bb04cbd43d70035e8d8
James Finlay <jtfinlay@ualberta.ca>
2014-11-10 11:41:16 -0700

    View & controller

---
+++ Assignment4/driver.rb
@@ -0,0 +1,5 @@
+# Note - Need Ruby 1.9.3
+
+require 'gtk2'
+
+v = View.new(20,20)
---
+++ Assignment4/view.rb
@@ -0,0 +1,34 @@
+# encoding: utf-8
+#
+# = view.rb
+#
+# Manages Gtk GUI
+#
+# Authors: Evan Degraff, James Finlay
+##
+
+require 'gtk2'
+
+class View
+
+  def initialize(width, height)
+    Gtk.init
+    window = Gtk::Window.new
+    window.border_width = 10
+
+    v = Gtk::VBox.new
+    Array.new(height).each_with_index{|a,row|
+      h = Gtk::HBox.new
+      Array.new(width).each_with_index{|b,col|
+        h.pack_start(Gtk::Button.new("   "))
+      }
+      v.pack_start(h)
+    }
+    window.add(v)
+
+    window.show_all
+    Gtk.main
+  end
+
+
+end
