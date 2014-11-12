bb083b27f52d650f29348770733369cbe9617fbd
azure provisioned user <azureuser@jameslinux.jameslinux.d5.internal.cloudapp.net>
2014-11-12 20:30:48 +0000

    Draws images

--- Assignment4/view.rb
+++ Assignment4/view.rb
@@ -14,13 +14,14 @@ class View
   def initialize(width, height)
     Gtk.init
     window = Gtk::Window.new
+    window.signal_connect("destroy"){Gtk.main_quit}
     window.border_width = 10
 
     v = Gtk::VBox.new
     Array.new(height).each_with_index{|a,row|
       h = Gtk::HBox.new
       Array.new(width).each_with_index{|b,col|
-        h.pack_start(Gtk::Button.new("   "))
+        h.pack_start(Gtk::Image.new("X.png"))
       }
       v.pack_start(h)
     }
