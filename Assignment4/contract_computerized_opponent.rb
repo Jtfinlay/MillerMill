76b156a3a1144d58e0814d501216f460f86b3179
James Finlay <jtfinlay@ualberta.ca>
2014-11-13 13:19:59 -0700

    AbstractListener & empty Computerized Opponent Contracts

---
+++ Assignment4/contract_computerized_opponent.rb
@@ -0,0 +1,29 @@
+
+
+require 'test/unit/assertions'
+
+module ContractGameBoard
+  include Test::Unit::Assertions
+
+  def class_invariant
+  end
+
+  def pre_initialize(difficulty)
+  end
+
+  def post_initialize
+  end
+
+  def pre_make_move(game_board)
+  end
+
+  def post_make_move
+  end
+
+  def pre_easy_difficulty(game_board)
+  end
+
+  def post_easy_difficulty
+  end
+
+end
