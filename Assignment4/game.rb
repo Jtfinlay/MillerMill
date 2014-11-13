94fea70fca71caaa8d6781f1cb1a17fdf0517981
Evan DeGraff <evandegraff@gmail.com>
2014-11-13 21:54:55 +0000

    Started win conditions

--- Assignment4/game.rb
+++ Assignment4/game.rb
@@ -37,7 +37,7 @@ class Game
     row = @board.col(column).find_index(0)
     @board[row,column] = @tokens[@turn-1]
     @turn = (@turn == 1) ? 2 : 1
-    check_win_conditions #TODO win condition stuff
+    check_win_conditions(row, column) #TODO win condition stuff
     @observers.each{|o| o.update_value(column,row,@board[row,column])}
   end
 
@@ -51,8 +51,18 @@ class Game
     add_to_column(@computerized_opponent.make_move(@board))
   end
 
-  def check_win_conditions
-
+  def check_win_conditions(row, column)
+    @board.row(0..-1).each{ |r|
+      r.each_index{ |i|
+        #puts "P#{@turn} wins!" if r[i] != 0 and r[i] == r[i+1] and r[i] == r[i+2] and r[i] == r[i+3]
+      }
+    }
+    (0..@board.row(0).size - 1).each{ |k]
+      c = @board.col(k)
+      c.each_index{ |i|
+        #puts "P#{@turn} wins!" if c[i] != 0 and c[i] == c[i+1] and c[i] == c[i+2] and c[i] == c[i+3]
+      }
+    }
   end
 
   def add_observer(view)
