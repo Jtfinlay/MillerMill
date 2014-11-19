8e923940ab704a3bfe5eec23308dc38b9b7af4d2
James Finlay <jtfinlay@ualberta.ca>
2014-11-19 11:57:16 -0700

    threading

--- Assignment4/driver.rb
+++ Assignment4/driver.rb
@@ -3,11 +3,15 @@
 require './game_controller'
 
 c = GameController.new
+
+Thread.new {
+  while true
+    print ">"
+    cmd = gets.chomp
+
+    # DO SHIT
+  end
+}
+
+
 c.start_game
-
-while true
-  print ">"
-  cmd = gets.chomp
-
-  # DO SHIT
-end
